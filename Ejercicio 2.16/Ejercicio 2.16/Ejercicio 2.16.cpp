63629a4536564941d2da96658a0c65f1344953b2
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2020-04-25 01:54:41 -0500

    1ra confirmación de Cambios

---
+++ Ejercicio 2.16/Ejercicio 2.16/Ejercicio 2.16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 0, b = 0;
+	cout << "Digite un numero entero A: ";
+	cin >> a;
+	cout << "Digite un numero entero B: ";
+	cin >> b;
+	cout << "\nLa suma de los numeros es: " << a + b << endl;
+	cout << "El producto de los numeros es: " << a * b << endl;
+	cout << "La diferencia de los numeros es: " << a - b << endl;
+	cout << "El cociente de los dos numeros es: " << a / b << endl;
+	return 0;
+]
