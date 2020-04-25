b48fbffeef9861eef5ca2a5988db753e18469717
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2020-04-25 02:14:06 -0500

    Confirmo cambio de E2.17.

---
+++ Ejercicio 2.17/Ejercicio 2.17/Ejercicio 2.17.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "a)" << endl;
+	cout << "VX" << " YZ" <<endl ;
+	cout << "b)" << endl;
+	cout << "V" << "X" << " Y" << "Z" << endl;
+	cout << "c)" << endl;
+	cout << "VX";
+	cout << " YZ" << endl;
+	return 0;
+}
